1134443265040f873438c9a47eebbe0372a41da7
luozhitao8200 <317944010@qq.com>
2024-12-28 14:22:39 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// LeanCloud 配置
+AV.init({
+    appId: "UVk0dXM9MBwsGGw4k8QVHDt6-MdYXbMMI",  // 示例 AppID
+    appKey: "UtOGWbGwgGmTqVW7YOGL0d7L", // 示例 AppKey
+    serverURL: "https://uvk0dxm9.lc-cn-n1-shared.com" // 示例服务器地址
+}); 
